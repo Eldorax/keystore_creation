abcfbd17acf30aa19776b2c7e38010cc32dba74f
Seb <l@ol.com>
2016-10-24 18:40:07 +0200

    last

--- src/main/java/GenKeystore.java
+++ src/main/java/GenKeystore.java
@@ -2,55 +2,91 @@ import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.KeyPairGenerator;
 import java.security.KeyPair;
+import java.security.Security;
+import java.math.BigInteger;
+import java.util.Date;
+
+import javax.security.auth.x500.X500Principal;
+import java.security.cert.X509Certificate;
+
+import org.bouncycastle.x509.extension.SubjectKeyIdentifierStructure;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
+import org.bouncycastle.asn1.x509.X509Extensions;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
 //Source : http://docs.oracle.com/javase/6/docs/api/java/security/KeyStore.html
-//		   http://docs.oracle.com/javase/6/docs/api/java/security/KeyStore.PrivateKeyEntry.html
+//	   http://docs.oracle.com/javase/6/docs/api/java/security/KeyStore.PrivateKeyEntry.html
 //         http://stackoverflow.com/questions/10956956/using-rsa-encryption-in-java-without-bouncycastle
-
+//	   http://www.bouncycastle.org/wiki/display/JA1/X.509+Public+Key+Certificate+and+Certification+Request+Generation
 
 
 public class GenKeystore {
 
+	
+
 	public static void main(String[] args) throws Exception {
 
-		//Création du keystore.
-		KeyStore ks = KeyStore.getInstance("jks");
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 
 		//Mot de passe.
 		char[] password = {'1', '2', '3', '4', '5', '6'};
 		KeyStore.PasswordProtection protected_password = new KeyStore.PasswordProtection(password);
-	
-		//Chargement à partir de rien (creation du keystore).
-		ks.load(null, password);
 
-		/*
-		//Création de la clef privée.
-		KeyStore.PrivateKeyEntry pkEntry = (KeyStore.PrivateKeyEntry)
-       										ks.getEntry("privateKeyAlias", protected_password);
-		PrivateKey myPrivateKey = pkEntry.getPrivateKey();
-		*/
+		//Création du keystore.
+		KeyStore ks = KeyStore.getInstance("jks");
+		ks.load(null, password);  //Chargement à partir de rien (creation du keystore et non importation).
 
 		//Création de la paire de clef.
 		KeyPairGenerator key_gen = KeyPairGenerator.getInstance("RSA");		
 		key_gen.initialize(1048);
 		KeyPair keys = key_gen.genKeyPair();
-		byte[] private_key = keys.getPrivate().getEncoded(); //clef privée encodé.
+		//byte[] private_key = keys.getPrivate().getEncoded(); //clef privée encodé.
 
-		//Sauvegarde de la clef privée.
-		//KeyStore.PrivateKeyEntry pkEntry = new KeyStore.PrivateKeyEntry(keys.getPrivate());
+
+		//Création du certificat.
+		//ContentSigner sigGen = new JcaContentSignerBuilder("SHA1withRSA").setProvider("BC").build(keys.getPrivate());
+		X509V3CertificateGenerator cert_gen = new X509V3CertificateGenerator();
+
+		X500Principal cn = new X500Principal("CN=SXP");
+		cert_gen.setSerialNumber(new BigInteger("123456789"));
+		cert_gen.setIssuerDN(cn);
+		cert_gen.setNotBefore(new Date());
+		cert_gen.setNotAfter(new Date());
+		cert_gen.setSubjectDN(cn);
+		cert_gen.setPublicKey(keys.getPublic());
+		cert_gen.setSignatureAlgorithm("MD5WithRSA");
 
 		/*
-		//Sauvegarde de la clef privée.
-		javax.crypto.SecretKey my_secret_key; //= new levraiquifaitleschose(); //on doit la créer ici ?
-    	KeyStore.SecretKeyEntry sk_entry = new KeyStore.SecretKeyEntry(my_secret_key);
-    	ks.setEntry("secretKeyAlias", sk_entry, protected_password);
-		*/
-/*
-	 void	setKeyEntry(String alias, byte[] key, Certificate[] chain) 
-          Assigns the given key (that has already been protected) to the given alias.
- void	setKeyEntry(String alias, Key key, char[] password, Certificate[] chain) 
-          Assigns the given key to the given alias, protecting it with the given password.
-*/
+		certGen.addExtension(X509Extensions.AuthorityKeyIdentifier, false,new
+		AuthorityKeyIdentifierStructure(caCert));
 
+		cert_gen.addExtension(X509Extensions.SubjectKeyIdentifier, false,
+		    new SubjectKeyIdentifierStructure( keys.getPublic().getEncoded() ));
+		*/
+
+		X509Certificate[] cert_chain = new X509Certificate[1];
+		cert_chain[0] = cert_gen.generateX509Certificate(keys.getPrivate(), "BC"); // CA private key (auto signed)
+
+		ks.setEntry("SXP",
+				new KeyStore.PrivateKeyEntry(keys.getPrivate(), cert_chain),
+				new KeyStore.PasswordProtection(password));
+
+
+		//Enregistement du keystore dans un fichier.
+		java.io.FileOutputStream fos = null;
+		try 
+		{
+			fos = new java.io.FileOutputStream("keystore.jks");
+			ks.store(fos, password);
+		}
+		finally 
+		{
+			if(fos != null)
+				fos.close();
+		}
+
+
+		
     }
 }
